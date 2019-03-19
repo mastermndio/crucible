bd7ff1fab396188a13e6abdc90d81e117b2cc44b
mastermnd <abrooks@mastermnd.io>
2019-03-18 21:05:30 -0400

    Completed exercism scrabble challenge with golang

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,47 @@
+package scrabble
+
+import (
+	"strings"
+)
+
+//Score is a function that calculates scrabble score
+func Score(word string) int {
+
+	var letterValue = map[string]int{
+		"A": 1,
+		"B": 3,
+		"C": 3,
+		"D": 2,
+		"E": 1,
+		"F": 4,
+		"G": 2,
+		"H": 4,
+		"I": 1,
+		"J": 8,
+		"K": 5,
+		"L": 1,
+		"M": 3,
+		"N": 1,
+		"O": 1,
+		"P": 3,
+		"Q": 10,
+		"R": 1,
+		"S": 1,
+		"T": 1,
+		"U": 1,
+		"V": 4,
+		"W": 4,
+		"X": 8,
+		"Y": 4,
+		"Z": 10,
+	}
+
+	wordUppercase := strings.ToUpper(word)
+	total := 0
+
+	for _, letter := range wordUppercase {
+		total += letterValue[string(letter)]
+	}
+
+	return total
+}
